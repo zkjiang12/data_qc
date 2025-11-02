da377bf4dc36a313ea96425b1ed5ab455c4eb665
Zikang Jiang <zkjiang12@gmail.com>
2025-11-02 13:46:27 +0800

    the hand detection works and its pretty fast. v2 is w older code that isn't used much now. the detectHands is p simple, it just makes sense. I had some trouble with it since I thought downloading the model would take a while/be confusing so I avoided it and sought work arounds, turns out it was very chill. then when I was running it, I got tons of empty outputs. I thought this was a bug in my code and that it should have been outputting values or at least outputing something to indicate it didn't find any hands. untrue. the output is just blank if it didn't find any hands. duh. just had to run it longer.

--- qc.py
+++ qc.py
@@ -5,15 +5,75 @@
 
 # finally I want to visualize some good examples and bad examples such that I can evaluate the labeling. 
 
-#STEP 1: video length
+#for getting vid length
 import ffmpeg # to read vid length
 from pathlib import Path #to access files and folders
 import time # to track efficiency/speed
+
+#for doing hand detection
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import cv2
 
 def getVidLength(path):
     out = ffmpeg.probe(path)
     clip_length = float(out['streams'][0]['duration']) #returned as a string, must convert to float to be usable later on.
     return clip_length/60; # convert to minutes
+
+def detectHands(vidPath): # this is the core part of the QC. goal is a percentage of frames with hands detected.
+    print(f'running hand detection on {vidPath}')
+    model_path = "/Users/zikangjiang/data-qc/hand_landmarker.task"
+
+    # STEP 2: Create an HandLandmarker object.
+    base_options = python.BaseOptions(model_asset_path=model_path)
+    options = vision.HandLandmarkerOptions(base_options=base_options,
+                                        num_hands=2)
+    detector = vision.HandLandmarker.create_from_options(options)
+
+    # Use OpenCV’s VideoCapture to load the input video.
+    clip = cv2.VideoCapture(vidPath)
+
+    # Loop through each frame in the video using VideoCapture#read()
+    frame_index = 0
+    while True:
+        #read the video
+        ret, frame = clip.read()     
+
+        if ret == False:
+            break;   
+
+        #need to convert image into an Image object that can be used in media pipe
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_rgb = cv2.resize(frame_rgb, (192, 192))
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb) 
+        detection_result = detector.detect(mp_image,)
+        print(detection_result)
+
+        frame_index += 1
+
+def detectHandsv2(vidPath):
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
+
+    # Use OpenCV’s VideoCapture to load the input video.
+    clip = cv2.VideoCapture(vidPath)
+
+    while True:
+        #read the video
+        ret, frame = clip.read()     
+
+        if ret == False:
+            break;   
+
+        # Convert the frame to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame with MediaPipe Pose
+        result = pose.process(frame_rgb)
+        print(result)
+        if result.pose_landmarks:
+            print(result.pose_landmarks.landmark)
 
 def main():
     parent_folder = Path("/Volumes") #creates a Path object
@@ -24,14 +84,18 @@ def main():
             video_folder = Path(f"/Volumes/{folder_name}/DCIM")
             vidlength = 0
             for video_files in video_folder.iterdir(): #gets all the clips on the SD card.
+                print(video_files)
+                print(type(video_files))
+                detectHands(video_files)
                 vidlength += getVidLength(video_files)*10
             print(f"{folder_name} is {round(vidlength/60*10)/10} hours long" )
 
 if __name__ == "__main__":
     start = time.time()
+    print("starting")
     main()
     end = time.time()
-    duration = end-start
+    duration = end - start
     print(f"took {round(duration/60*10)/10} minutes")
 
 # results from running the above
