07a2ebe276a62def0987e56e80dd83023facbc72
Zikang Jiang <zkjiang12@gmail.com>
2025-11-02 13:49:27 +0800

    added some comments. made it cleaner

--- qc.py
+++ qc.py
@@ -35,11 +35,11 @@ def detectHands(vidPath): # this is the core part of the QC. goal is a percentag
     clip = cv2.VideoCapture(vidPath)
 
     # Loop through each frame in the video using VideoCapture#read()
-    frame_index = 0
     while True:
         #read the video
         ret, frame = clip.read()     
 
+        #if no more frames in the video
         if ret == False:
             break;   
 
@@ -47,11 +47,14 @@ def detectHands(vidPath): # this is the core part of the QC. goal is a percentag
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_rgb = cv2.resize(frame_rgb, (192, 192))
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb) 
+
+        #run the detection model
         detection_result = detector.detect(mp_image,)
         print(detection_result)
 
-        frame_index += 1
 
+#code I wrotte since I thought the above hand detection wasn't working. 
+#it's pretty similar to the above, you don't need to download a model, but also the output has a worse format.
 def detectHandsv2(vidPath):
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose()
@@ -75,21 +78,26 @@ def detectHandsv2(vidPath):
         if result.pose_landmarks:
             print(result.pose_landmarks.landmark)
 
+#main QC code that loops through the SD cards and runs QC on each video. 
 def main():
     parent_folder = Path("/Volumes") #creates a Path object
     #finds all the SD cards currently being read by the computer
     for subdir in parent_folder.iterdir(): #calls method iterdir() that gets all the sub directories
         folder_name = subdir.name
-        if "Untitled" in folder_name: #checks that its an SD card folder.
+        #checks that its an SD card folder.
+        if "Untitled" in folder_name: 
             video_folder = Path(f"/Volumes/{folder_name}/DCIM")
             vidlength = 0
-            for video_files in video_folder.iterdir(): #gets all the clips on the SD card.
+            #gets all the clips on the SD card.
+            for video_files in video_folder.iterdir(): 
                 print(video_files)
                 print(type(video_files))
                 detectHands(video_files)
                 vidlength += getVidLength(video_files)*10
             print(f"{folder_name} is {round(vidlength/60*10)/10} hours long" )
 
+#only runs if this is the file being run, not imported into another file. 
+#really just added it here for vibes to make the code look nicer.
 if __name__ == "__main__":
     start = time.time()
     print("starting")
@@ -97,10 +105,3 @@ if __name__ == "__main__":
     end = time.time()
     duration = end - start
     print(f"took {round(duration/60*10)/10} minutes")
-
-# results from running the above
-# zikangjiang@Zikangs-MacBook-Pro data-qc % python3 qc.py
-# Untitled 2 is 7.759229722222224 hours long
-# Untitled 1 is 7.446141388888889 hours long
-# Untitled is 7.448672500000001 hours long
-# took 107.08723092079163
